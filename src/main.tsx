afe217e9dc3553704e1d3f9745331818323fec17
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-08 20:20:30 -0300

    :sparkles: feat: Adiciona componente de rotas
    Importa arquivos de estilização e adiciona componente de rotas.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@/sass/_reset.scss'
+import '@/sass/_global.scss'
+import AppRoutes from '@/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode></React.StrictMode>
+    <React.StrictMode>
+        <AppRoutes />
+    </React.StrictMode>
 )
