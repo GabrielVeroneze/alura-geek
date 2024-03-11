a822111ea237b0fd092071a4cf0dee60d28c5d72
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-10 22:54:47 -0300

    :sparkles: feat: Cria componente de grupo de produtos

---
+++ src/components/GrupoProdutos/index.tsx
@@ -0,0 +1,23 @@
+import { IProduto } from '@/interfaces/IProduto'
+import Produto from './Produto'
+import styles from './GrupoProdutos.module.scss'
+
+interface GrupoProdutosProps {
+    categoria: string
+    produtos: IProduto[]
+}
+
+const GrupoProdutos = ({ categoria, produtos }: GrupoProdutosProps) => {
+    return (
+        <section className={styles.grupo}>
+            <div className={styles.topo}>
+                <h3 className={styles.categoria}>{categoria}</h3>
+                <button className={styles.botao}>Ver tudo</button>
+            </div>
+            <ul className={styles.lista}>
+            </ul>
+        </section>
+    )
+}
+
+export default GrupoProdutos
