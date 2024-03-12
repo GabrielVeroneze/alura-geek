4bbccd43554f73890aae857646471e1a91fc319b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-12 17:39:52 -0300

    :sparkles: feat: Cria página inicial

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import { useManipularProdutos } from '@/hooks/useManipularProdutos'
+import { filtrarProdutosPorCategoria, obterCategoriasUnicas } from '@/utils/operacoesCategorias'
+import Cabecalho from '@/components/Cabecalho'
+import GrupoProdutos from '@/components/GrupoProdutos'
+import Banner from './Banner'
+import styles from './Home.module.scss'
+
+const Home = () => {
+    const { produtos } = useManipularProdutos()
+    const categorias = obterCategoriasUnicas(produtos)
+
+    return (
+        <>
+            <Cabecalho />
+            <Banner />
+            <main className={styles.principal}>
+                {categorias.map(categoria => (
+                    <GrupoProdutos
+                        key={self.crypto.randomUUID()}
+                        categoria={categoria}
+                        produtos={filtrarProdutosPorCategoria(
+                            produtos,
+                            categoria
+                        )}
+                    />
+                ))}
+            </main>
+        </>
+    )
+}
+
+export default Home
