5322829973a262b677adda2cb29dede0a4cbe4aa
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-14 23:01:42 -0300

    :sparkles: feat: Cria rota para página de login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Home from '@/pages/Home'
+import Login from '@/pages/Login'
 
 const AppRoutes = () => {
     return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
                 </Route>
             </Routes>
         </Router>
