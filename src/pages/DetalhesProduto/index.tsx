54887b4af4bb22fcc798b48bb8de15bfb2391876
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-16 19:48:15 -0300

    :sparkles: feat: Cria página de detalhes do produto

---
+++ src/pages/DetalhesProduto/index.tsx
@@ -0,0 +1,27 @@
+import { useObterProduto } from '@/hooks/useObterProduto'
+import styles from './DetalhesProduto.module.scss'
+
+const DetalhesProduto = () => {
+    const { produto } = useObterProduto()
+
+    return (
+        <main className={styles.principal}>
+            <section className={styles.produto}>
+                <div className={styles.wrapper}>
+                    <img
+                        className={styles.imagem}
+                        src={`${window.location.origin}/${produto?.imagem}`}
+                        alt={produto?.nome}
+                    />
+                </div>
+                <div className={styles.info}>
+                    <h4 className={styles.nome}>{produto?.nome}</h4>
+                    <p className={styles.preco}>R$ {produto?.preco}</p>
+                    <p className={styles.descricao}>{produto?.descricao}</p>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default DetalhesProduto
