fe93a2e01f9b25b361bad295465c7017a887e081
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-16 16:46:23 -0300

    :sparkles: feat: Cria rota para página de detalhes do produto

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Home from '@/pages/Home'
 import Login from '@/pages/Login'
+import DetalhesProduto from '@/pages/DetalhesProduto'
 
 const AppRoutes = () => {
     return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="login" element={<Login />} />
+                    <Route path="produto/:id" element={<DetalhesProduto />} />
                 </Route>
             </Routes>
         </Router>
