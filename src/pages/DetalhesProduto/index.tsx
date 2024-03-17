229d18f4ce78a22e1241e71cbac7aba2bf127791
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-17 18:50:47 -0300

    :bug: fix: Corrige erro de produto ser possivelmente 'null'

--- src/pages/DetalhesProduto/index.tsx
+++ src/pages/DetalhesProduto/index.tsx
@@ -3,6 +3,9 @@ import styles from './DetalhesProduto.module.scss'
 
 const DetalhesProduto = () => {
     const { produto } = useObterProduto()
+    if (!produto) {
+        return null
+    }
 
     return (
         <main className={styles.principal}>
@@ -10,14 +13,14 @@ const DetalhesProduto = () => {
                 <div className={styles.wrapper}>
                     <img
                         className={styles.imagem}
-                        alt={produto?.nome}
                         src={produto.imagem}
+                        alt={produto.nome}
                     />
                 </div>
                 <div className={styles.info}>
-                    <h4 className={styles.nome}>{produto?.nome}</h4>
-                    <p className={styles.preco}>R$ {produto?.preco}</p>
-                    <p className={styles.descricao}>{produto?.descricao}</p>
+                    <h4 className={styles.nome}>{produto.nome}</h4>
+                    <p className={styles.preco}>R$ {produto.preco}</p>
+                    <p className={styles.descricao}>{produto.descricao}</p>
                 </div>
             </section>
         </main>
