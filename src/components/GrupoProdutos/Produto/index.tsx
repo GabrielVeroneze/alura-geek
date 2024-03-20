6f790746e392e971eefa8f0c5ba10815a25a2e98
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-20 16:06:13 -0300

    :sparkles: feat: Adiciona botões de editar e excluir
    Adapta o componente para renderizar os modelos de visualização e edição. O modelo "editar" adiciona botões de editar e excluir o produto.

--- src/components/GrupoProdutos/Produto/index.tsx
+++ src/components/GrupoProdutos/Produto/index.tsx
@@ -1,15 +1,36 @@
 import { IProduto } from '@/interfaces/IProduto'
 import styles from './Produto.module.scss'
 
-const Produto = ({ imagem, nome, preco }: IProduto) => {
+interface ProdutoProps {
+    modelo: 'visualizar' | 'editar'
+    produto: IProduto
+}
+
+const Produto = ({ modelo, produto }: ProdutoProps) => {
     return (
         <li className={styles.card}>
             <div className={styles.wrapper}>
-                <img className={styles.imagem} src={imagem} alt={nome} />
+                {modelo === 'editar' && (
+                    <div className={styles.icones}>
+                        <button
+                            className={styles.excluir}
+                            aria-label="Excluir"
+                        ></button>
+                        <button
+                            className={styles.editar}
+                            aria-label="Editar"
+                        ></button>
+                    </div>
+                )}
+                <img
+                    className={styles.imagem}
+                    src={produto.imagem}
+                    alt={produto.nome}
+                />
             </div>
             <div className={styles.info}>
-                <h4 className={styles.nome}>{nome}</h4>
-                <p className={styles.preco}>R$ {preco}</p>
+                <h4 className={styles.nome}>{produto.nome}</h4>
+                <p className={styles.preco}>R$ {produto.preco}</p>
                 <button className={styles.botao}>Ver produto</button>
             </div>
         </li>
