44b35572053eef7d93e240c3c70e00e7a1af231f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-24 17:31:29 -0300

    :sparkles: feat: Cria átomo dos produtos

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -1,4 +1,12 @@
 import { atom } from 'recoil'
+import { produtosAsyncSelector } from '@/state/selectors'
+import { IProduto } from '@/interfaces/IProduto'
+
+export const produtosAtom = atom<IProduto[]>({
+    key: 'produtosAtom',
+    default: produtosAsyncSelector,
+})
+
 export const termoDePesquisaAtom = atom<string>({
     key: 'termoDePesquisaAtom',
     default: '',
