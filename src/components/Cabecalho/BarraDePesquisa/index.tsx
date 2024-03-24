e1a667225c315e40fc47260f2d12b8ba58d96420
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-24 20:06:08 -0300

    :sparkles: feat: Adiciona eventos para realizar pesquisa
    Redireciona usuário para página de resultados quando a tecla "Enter" for pressionada ou quando o ícone de lupa for clicado.

--- src/components/Cabecalho/BarraDePesquisa/index.tsx
+++ src/components/Cabecalho/BarraDePesquisa/index.tsx
@@ -1,8 +1,16 @@
 import { useTermoDePesquisa } from '@/hooks/useTermoDePesquisa'
+import { usePesquisarProdutos } from '@/hooks/usePesquisarProdutos'
 import styles from './BarraDePesquisa.module.scss'
 
 const BarraDePesquisa = () => {
     const { termo, setTermo } = useTermoDePesquisa()
+    const { redirecionarParaResultados } = usePesquisarProdutos()
+
+    const handleKeyDown = (evento: React.KeyboardEvent<HTMLInputElement>) => {
+        if (evento.key === 'Enter') {
+            redirecionarParaResultados()
+        }
+    }
 
     return (
         <div className={styles.barra}>
@@ -12,8 +20,12 @@ const BarraDePesquisa = () => {
                 placeholder="O que deseja encontrar?"
                 value={termo}
                 onChange={evento => setTermo(evento.target.value)}
+                onKeyDown={evento => handleKeyDown(evento)}
             />
-            <button className={styles.lupa}></button>
+            <button
+                className={styles.lupa}
+                onClick={() => redirecionarParaResultados()}
+            ></button>
         </div>
     )
 }
