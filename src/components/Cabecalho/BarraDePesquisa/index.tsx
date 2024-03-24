190fa0a4e6e732d05980551865511ccb53d57bc7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-24 19:44:23 -0300

    :sparkles: feat: Adicionar estado ao input de pesquisa

--- src/components/Cabecalho/BarraDePesquisa/index.tsx
+++ src/components/Cabecalho/BarraDePesquisa/index.tsx
@@ -1,12 +1,17 @@
+import { useTermoDePesquisa } from '@/hooks/useTermoDePesquisa'
 import styles from './BarraDePesquisa.module.scss'
 
 const BarraDePesquisa = () => {
+    const { termo, setTermo } = useTermoDePesquisa()
+
     return (
         <div className={styles.barra}>
             <input
                 className={styles.input}
                 type="search"
                 placeholder="O que deseja encontrar?"
+                value={termo}
+                onChange={evento => setTermo(evento.target.value)}
             />
             <button className={styles.lupa}></button>
         </div>
