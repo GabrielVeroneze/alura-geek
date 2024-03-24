412abfb5971e52d8d3210bfcd814b0fdc6252039
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-23 22:56:55 -0300

    :sparkles: feat: Usa o <RecoilRoot> para fornecer o contexto

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
 import AppRoutes from '@/routes'
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <AppRoutes />
+        <RecoilRoot>
+            <AppRoutes />
+        </RecoilRoot>
     </React.StrictMode>
 )
