4c65f155791bc1e443dafd0c6a624bdd89db0226
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-24 17:35:21 -0300

    :sparkles: feat: Cria estado derivado de produtos
    Cria estado derivado de produtos que faz uma filtragem nos produtos de acordo com o termo de pesquisa, e retorna um array com os produtos filtrados.

--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -1,4 +1,5 @@
 import { selector } from 'recoil'
+import { produtosAtom, termoDePesquisaAtom } from '@/state/atoms'
 import { IProduto } from '@/interfaces/IProduto'
 import http from '@/http'
 
@@ -9,5 +10,21 @@ export const produtosAsyncSelector = selector<IProduto[]>({
         const dados = resposta.data
 
         return dados
+    },
+})
+
+export const produtosPesquisadosSelector = selector<IProduto[]>({
+    key: 'produtosPesquisadosSelector',
+    get: ({ get }) => {
+        const produtos = get(produtosAtom)
+        const termo = get(termoDePesquisaAtom)
+
+        const regex = new RegExp(termo, 'i')
+
+        const resultadoPesquisa = produtos.filter(produto =>
+            regex.test(produto.nome)
+        )
+
+        return resultadoPesquisa
     },
 })
