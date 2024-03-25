acd3d6b866a61c5b76c7a4f7ce92bcc8f20f993d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-25 17:55:28 -0300

    :sparkles: feat: Utiliza o <Suspense>
    Utiliza o <Suspense> para lidar com o carregamento assíncrono nos componentes

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
@@ -8,7 +8,9 @@ import { RecoilRoot } from 'recoil'
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <RecoilRoot>
-            <AppRoutes />
+            <Suspense>
+                <AppRoutes />
+            </Suspense>
         </RecoilRoot>
     </React.StrictMode>
 )
