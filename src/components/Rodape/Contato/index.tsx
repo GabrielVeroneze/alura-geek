5add73ee2e91f3b6307aa0730552544fe4f156d9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-26 23:17:43 -0300

    :sparkles: feat: Utiliza hook para manipular formulario
    Utiliza hook customizado para manipular os campos e o envio do formulario

--- src/components/Rodape/Contato/index.tsx
+++ src/components/Rodape/Contato/index.tsx
@@ -1,18 +1,37 @@
+import { useFormularioContato } from '@/hooks/useFormularioContato'
 import styles from './Contato.module.scss'
 
 const Contato = () => {
+    const { dadosContato, handleDadosChange, handleSubmit } = useFormularioContato()
+
     return (
-        <form className={styles.formulario}>
+        <form
+            className={styles.formulario}
+            onSubmit={evento => handleSubmit(evento)}
+        >
             <h3 className={styles.titulo}>Fale conosco</h3>
             <div className={styles.campo}>
                 <label htmlFor="nome">Nome</label>
-                <input type="text" id="nome" />
+                <input
+                    type="text"
+                    id="nome"
+                    value={dadosContato.nome}
+                    onChange={evento =>
+                        handleDadosChange('nome', evento.target.value)
+                    }
+                />
             </div>
             <textarea
                 className={styles.mensagem}
                 placeholder="Escreva sua mensagem"
+                value={dadosContato.mensagem}
+                onChange={evento =>
+                    handleDadosChange('mensagem', evento.target.value)
+                }
             ></textarea>
-            <button className={styles.botao}>Enviar mensagem</button>
+            <button className={styles.botao} type="submit">
+                Enviar mensagem
+            </button>
         </form>
     )
 }
