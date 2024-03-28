292821272f521a6752c0d328c71be6425c90a4b9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-28 16:02:48 -0300

    :sparkles: feat: Adiciona os tipos de erros

---
+++ src/interfaces/IMensagensDeErro.ts
@@ -0,0 +1,12 @@
+export type Erro =
+    | 'badInput'
+    | 'customError'
+    | 'patternMismatch'
+    | 'rangeOverflow'
+    | 'rangeUnderflow'
+    | 'stepMismatch'
+    | 'tooLong'
+    | 'tooShort'
+    | 'typeMismatch'
+    | 'valueMissing'
+
