84f7422fd711a059e34b8eb64418a396055d4bd4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-03-28 19:52:11 -0300

    :sparkles: feat: Cria hook customizado
    Cria hook e adiciona os tipos e mensagens de erro.

---
+++ src/hooks/useValidarFormularioContato.ts
@@ -0,0 +1,20 @@
+import { Erro, IMensagensDeErro } from '@/interfaces/IMensagensDeErro'
+
+export const useValidarFormularioContato = () => {
+    const tiposDeErro: Erro[] = ['tooLong', 'tooShort', 'valueMissing']
+
+    const mensagensDeErro: IMensagensDeErro = {
+        nome: {
+            valueMissing: 'O campo de nome não pode estar vazio.',
+            tooShort: 'O campo de nome não tem caractéres suficientes.',
+            tooLong: 'O nome não pode ter mais de 40 caracteres.',
+        },
+        mensagem: {
+            valueMissing: 'O campo de mensagem não pode estar vazio.',
+            tooShort: 'O campo de mensagem não tem caractéres suficientes.',
+            tooLong:
+                'A mensagem não pode ultrapassar 120 caracteres. Diminua um pouco o texto!',
+        },
+    }
+
+}
