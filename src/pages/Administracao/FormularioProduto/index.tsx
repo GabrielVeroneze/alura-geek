8fb23a7b1f51d516b246b3138c24c303aa48dce2
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-01 15:49:33 -0300

    :sparkles: feat: Usa hook para manipular estado dos dados formulario
    Utiliza o estado produtoDados para definir os valores dos campos e a função handleDadosChange que atualiza o valor desse estado.

--- src/pages/Administracao/FormularioProduto/index.tsx
+++ src/pages/Administracao/FormularioProduto/index.tsx
@@ -1,7 +1,10 @@
+import { useFormularioProduto } from '@/hooks/useFormularioProduto'
 import CampoTextoFloatLabel from '@/components/CampoTextoFloatLabel'
 import styles from './FormularioProduto.module.scss'
 
 const FormularioProduto = () => {
+    const { produtoDados, handleDadosChange } = useFormularioProduto()
+
     return (
         <form className={styles.formulario}>
             <h2 className={styles.titulo}>Adicionar novo produto</h2>
@@ -11,6 +14,8 @@ const FormularioProduto = () => {
                     type: 'url',
                     id: 'imagem',
                     name: 'imagem',
+                    value: produtoDados.imagem,
+                    onChange: evento => handleDadosChange(evento),
                 }}
             />
             <CampoTextoFloatLabel
@@ -19,6 +24,8 @@ const FormularioProduto = () => {
                     type: 'text',
                     id: 'categoria',
                     name: 'categoria',
+                    value: produtoDados.categoria,
+                    onChange: evento => handleDadosChange(evento),
                 }}
             />
             <CampoTextoFloatLabel
@@ -27,6 +34,8 @@ const FormularioProduto = () => {
                     type: 'text',
                     id: 'nome',
                     name: 'nome',
+                    value: produtoDados.nome,
+                    onChange: evento => handleDadosChange(evento),
                 }}
             />
             <CampoTextoFloatLabel
@@ -35,11 +44,16 @@ const FormularioProduto = () => {
                     type: 'number',
                     id: 'preco',
                     name: 'preco',
+                    value: produtoDados.preco,
+                    onChange: evento => handleDadosChange(evento),
                 }}
             />
             <textarea
                 className={styles.descricao}
                 placeholder="Descrição do produto"
+                name="descricao"
+                value={produtoDados.descricao}
+                onChange={evento => handleDadosChange(evento)}
             ></textarea>
             <button className={styles.botao}>Adicionar produto</button>
         </form>
