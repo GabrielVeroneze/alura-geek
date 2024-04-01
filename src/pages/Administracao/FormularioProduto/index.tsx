1eee4061af8d43df104db704555c296160ba1055
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-01 14:39:53 -0300

    :recycle: refactor: Altera campos por componente CampoTextoFloatLabel

--- src/pages/Administracao/FormularioProduto/index.tsx
+++ src/pages/Administracao/FormularioProduto/index.tsx
@@ -1,25 +1,42 @@
+import CampoTextoFloatLabel from '@/components/CampoTextoFloatLabel'
 import styles from './FormularioProduto.module.scss'
 
 const FormularioProduto = () => {
     return (
         <form className={styles.formulario}>
             <h2 className={styles.titulo}>Adicionar novo produto</h2>
-            <div className={styles.campo}>
-                <label htmlFor="url">URL da imagem</label>
-                <input type="text" id="url" />
-            </div>
-            <div className={styles.campo}>
-                <label htmlFor="categoria">Categoria</label>
-                <input type="text" id="categoria" />
-            </div>
-            <div className={styles.campo}>
-                <label htmlFor="nome">Nome do produto</label>
-                <input type="text" id="nome" />
-            </div>
-            <div className={styles.campo}>
-                <label htmlFor="preco">Preço do produto</label>
-                <input type="number" id="preco" />
-            </div>
+            <CampoTextoFloatLabel
+                label="URL da imagem"
+                inputConfig={{
+                    type: 'url',
+                    id: 'imagem',
+                    name: 'imagem',
+                }}
+            />
+            <CampoTextoFloatLabel
+                label="Categoria"
+                inputConfig={{
+                    type: 'text',
+                    id: 'categoria',
+                    name: 'categoria',
+                }}
+            />
+            <CampoTextoFloatLabel
+                label="Nome do produto"
+                inputConfig={{
+                    type: 'text',
+                    id: 'nome',
+                    name: 'nome',
+                }}
+            />
+            <CampoTextoFloatLabel
+                label="Preço do produto"
+                inputConfig={{
+                    type: 'number',
+                    id: 'preco',
+                    name: 'preco',
+                }}
+            />
             <textarea
                 className={styles.descricao}
                 placeholder="Descrição do produto"
