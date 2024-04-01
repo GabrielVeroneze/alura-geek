6ab9aba680618136ca1272448956eb9fe3fa853a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-01 20:52:01 -0300

    :sparkles: feat: Exibe as mensagens de erro

--- src/pages/Administracao/FormularioProduto/index.tsx
+++ src/pages/Administracao/FormularioProduto/index.tsx
@@ -1,83 +1,109 @@
 import { useFormularioProduto } from '@/hooks/useFormularioProduto'
 import { useValidarFormularioProduto } from '@/hooks/useValidarFormularioProduto'
 import CampoTextoFloatLabel from '@/components/CampoTextoFloatLabel'
+import MensagemErro from '@/components/MensagemErro'
 import styles from './FormularioProduto.module.scss'
 
 const FormularioProduto = () => {
     const { produtoDados, handleDadosChange } = useFormularioProduto()
-    const { validarCampo } = useValidarFormularioProduto()
+    const { errosValidacao, validarCampo } = useValidarFormularioProduto()
 
     return (
         <form className={styles.formulario}>
             <h2 className={styles.titulo}>Adicionar novo produto</h2>
-            <CampoTextoFloatLabel
-                label="URL da imagem"
-                inputConfig={{
-                    type: 'url',
-                    id: 'imagem',
-                    name: 'imagem',
-                    value: produtoDados.imagem,
-                    onChange: evento => handleDadosChange(evento),
-                    onInvalid: evento => evento.preventDefault(),
-                    onBlur: evento => validarCampo(evento.target),
-                    pattern: '.+\\.(png|jpg)',
-                    required: true,
-                }}
-            />
-            <CampoTextoFloatLabel
-                label="Categoria"
-                inputConfig={{
-                    type: 'text',
-                    id: 'categoria',
-                    name: 'categoria',
-                    value: produtoDados.categoria,
-                    onChange: evento => handleDadosChange(evento),
-                    onInvalid: evento => evento.preventDefault(),
-                    onBlur: evento => validarCampo(evento.target),
-                    required: true,
-                }}
-            />
-            <CampoTextoFloatLabel
-                label="Nome do produto"
-                inputConfig={{
-                    type: 'text',
-                    id: 'nome',
-                    name: 'nome',
-                    value: produtoDados.nome,
-                    onChange: evento => handleDadosChange(evento),
-                    onInvalid: evento => evento.preventDefault(),
-                    onBlur: evento => validarCampo(evento.target),
-                    maxLength: 20,
-                    minLength: 3,
-                    required: true,
-                }}
-            />
-            <CampoTextoFloatLabel
-                label="Preço do produto"
-                inputConfig={{
-                    type: 'number',
-                    id: 'preco',
-                    name: 'preco',
-                    value: produtoDados.preco,
-                    onChange: evento => handleDadosChange(evento),
-                    onInvalid: evento => evento.preventDefault(),
-                    onBlur: evento => validarCampo(evento.target),
-                    min: 0,
-                    required: true,
-                }}
-            />
-            <textarea
-                className={styles.descricao}
-                placeholder="Descrição do produto"
-                name="descricao"
-                value={produtoDados.descricao}
-                onChange={evento => handleDadosChange(evento)}
-                onInvalid={evento => evento.preventDefault()}
-                onBlur={evento => validarCampo(evento.target)}
-                maxLength={150}
-                minLength={10}
-                required
-            ></textarea>
+            <fieldset>
+                <CampoTextoFloatLabel
+                    label="URL da imagem"
+                    inputConfig={{
+                        type: 'url',
+                        id: 'imagem',
+                        name: 'imagem',
+                        value: produtoDados.imagem,
+                        onChange: evento => handleDadosChange(evento),
+                        onInvalid: evento => evento.preventDefault(),
+                        onBlur: evento => validarCampo(evento.target),
+                        pattern: '.+\\.(png|jpg)',
+                        required: true,
+                    }}
+                />
+                {errosValidacao.imagem && (
+                    <MensagemErro>{errosValidacao.imagem}</MensagemErro>
+                )}
+            </fieldset>
+            <fieldset>
+                <CampoTextoFloatLabel
+                    label="Categoria"
+                    inputConfig={{
+                        type: 'text',
+                        id: 'categoria',
+                        name: 'categoria',
+                        value: produtoDados.categoria,
+                        onChange: evento => handleDadosChange(evento),
+                        onInvalid: evento => evento.preventDefault(),
+                        onBlur: evento => validarCampo(evento.target),
+                        required: true,
+                    }}
+                />
+                {errosValidacao.categoria && (
+                    <MensagemErro>{errosValidacao.categoria}</MensagemErro>
+                )}
+            </fieldset>
+            <fieldset>
+                <CampoTextoFloatLabel
+                    label="Nome do produto"
+                    inputConfig={{
+                        type: 'text',
+                        id: 'nome',
+                        name: 'nome',
+                        value: produtoDados.nome,
+                        onChange: evento => handleDadosChange(evento),
+                        onInvalid: evento => evento.preventDefault(),
+                        onBlur: evento => validarCampo(evento.target),
+                        maxLength: 20,
+                        minLength: 3,
+                        required: true,
+                    }}
+                />
+                {errosValidacao.nome && (
+                    <MensagemErro>{errosValidacao.nome}</MensagemErro>
+                )}
+            </fieldset>
+            <fieldset>
+                <CampoTextoFloatLabel
+                    label="Preço do produto"
+                    inputConfig={{
+                        type: 'number',
+                        id: 'preco',
+                        name: 'preco',
+                        value: produtoDados.preco,
+                        onChange: evento => handleDadosChange(evento),
+                        onInvalid: evento => evento.preventDefault(),
+                        onBlur: evento => validarCampo(evento.target),
+                        min: 0,
+                        required: true,
+                    }}
+                />
+                {errosValidacao.preco && (
+                    <MensagemErro>{errosValidacao.preco}</MensagemErro>
+                )}
+            </fieldset>
+            <fieldset>
+                <textarea
+                    className={styles.descricao}
+                    placeholder="Descrição do produto"
+                    name="descricao"
+                    value={produtoDados.descricao}
+                    onChange={evento => handleDadosChange(evento)}
+                    onInvalid={evento => evento.preventDefault()}
+                    onBlur={evento => validarCampo(evento.target)}
+                    maxLength={150}
+                    minLength={10}
+                    required
+                ></textarea>
+                {errosValidacao.descricao && (
+                    <MensagemErro>{errosValidacao.descricao}</MensagemErro>
+                )}
+            </fieldset>
             <button className={styles.botao}>Adicionar produto</button>
         </form>
     )
