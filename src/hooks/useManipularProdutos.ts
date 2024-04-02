ccd938b228d02594a4f613fd272d2e18aea60f65
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-01 23:13:53 -0300

    :sparkles: feat: Cria função para cadastrar novo produto
    Adiciona função para enviar os dados do novo produto, utilizando a instância axios "http" com o método POST.

--- src/hooks/useManipularProdutos.ts
+++ src/hooks/useManipularProdutos.ts
@@ -1,11 +1,23 @@
 import { useRecoilValue } from 'recoil'
 import { produtosAtom } from '@/state/atoms'
 import { IProduto } from '@/interfaces/IProduto'
+import http from '@/http'
 
 export const useManipularProdutos = () => {
     const produtos = useRecoilValue<IProduto[]>(produtosAtom)
 
+    const cadastrarProduto = (produto: IProduto) => {
+        http.post<IProduto>('produtos', produto)
+            .then(() => {
+                alert('Projeto adicionado com sucesso!')
+            })
+            .catch(() => {
+                alert('Erro ao cadastrar o projeto.')
+            })
+    }
+
     return {
         produtos,
+        cadastrarProduto,
     }
 }
