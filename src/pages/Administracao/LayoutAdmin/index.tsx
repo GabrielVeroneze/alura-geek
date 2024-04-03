bdef3590a95cb861bd8e6200efcfa838699ade0c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-02 21:06:45 -0300

    :sparkles: feat: Utiliza componente ToastContainer

--- src/pages/Administracao/LayoutAdmin/index.tsx
+++ src/pages/Administracao/LayoutAdmin/index.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import styles from './LayoutAdmin.module.scss'
+import 'react-toastify/dist/ReactToastify.css'
 
 const LayoutAdmin = () => {
     return (
         <main className={styles.principal}>
             <Outlet />
+            <ToastContainer />
         </main>
     )
 }
