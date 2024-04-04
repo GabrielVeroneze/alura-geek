62dfefdb707656bb68404c98f30d99611ed837d9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-03 22:59:32 -0300

    :sparkles: feat: Utiliza hook para manipular os inputs

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,10 @@
+import { useFormularioLogin } from '@/hooks/useFormularioLogin'
 import CampoTexto from '@/components/CampoTexto'
 import styles from './Login.module.scss'
 
 const Login = () => {
+    const { loginDados, handleDadosChange } = useFormularioLogin()
+
     return (
         <main className={styles.principal}>
             <h2 className={styles.titulo}>Iniciar Sessão</h2>
@@ -11,6 +14,8 @@ const Login = () => {
                     inputConfig={{
                         type: 'email',
                         name: 'email',
+                        value: loginDados.email,
+                        onChange: evento => handleDadosChange(evento),
                     }}
                 />
                 <CampoTexto
@@ -18,6 +23,8 @@ const Login = () => {
                     inputConfig={{
                         type: 'password',
                         name: 'senha',
+                        value: loginDados.senha,
+                        onChange: evento => handleDadosChange(evento),
                     }}
                 />
                 <button className={styles.botao}>Entrar</button>
