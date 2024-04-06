c11883ee98a1a4f7a617e9b2fd19d78097e7f873
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-05 22:49:32 -0300

    :sparkles: feat: Inicializa o emailjs com chave pública
    Adiciona inicialização do emailjs no arquivo main.tsx com a chave pública '1MAqJjgbgztL3hvIp' para configuração de envio de e-mails.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React, { Suspense } from 'react'
+import { RecoilRoot } from 'recoil'
+import emailjs from '@emailjs/browser'
 import ReactDOM from 'react-dom/client'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
 import AppRoutes from '@/routes'
-import { RecoilRoot } from 'recoil'
+
+emailjs.init({
+    publicKey: '1MAqJjgbgztL3hvIp'
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
