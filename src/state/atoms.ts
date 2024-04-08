909e881943b688e92b79f2a25197c8bdec4e2c7a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-07 23:25:18 -0300

    :sparkles: feat: Cria estado de usuário

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -1,6 +1,7 @@
 import { atom } from 'recoil'
 import { produtosAsyncSelector } from '@/state/selectors'
 import { IProduto } from '@/interfaces/IProduto'
+import { IUsuario } from '@/interfaces/IUsuario'
 
 export const produtosAtom = atom<IProduto[]>({
     key: 'produtosAtom',
@@ -10,4 +11,9 @@ export const produtosAtom = atom<IProduto[]>({
 export const termoDePesquisaAtom = atom<string>({
     key: 'termoDePesquisaAtom',
     default: '',
+})
+
+export const usuarioAtom = atom<IUsuario | null>({
+    key: 'usuarioAtom',
+    default: null,
 })
