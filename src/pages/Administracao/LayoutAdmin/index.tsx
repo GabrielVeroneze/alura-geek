e76ae96d18a7b57ebcd77120473356339a60c83d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-07 23:30:46 -0300

    :sparkles: feat: Deixa as rotas de admin privadas

--- src/pages/Administracao/LayoutAdmin/index.tsx
+++ src/pages/Administracao/LayoutAdmin/index.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useAutenticarUsuario } from '@/hooks/useAutenticarUsuario'
 import styles from './LayoutAdmin.module.scss'
 import 'react-toastify/dist/ReactToastify.css'
 
 const LayoutAdmin = () => {
+    const { usuario } = useAutenticarUsuario()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!usuario) {
+            navigate('/login')
+        }
+    }, [navigate, usuario])
+
     return (
         <main className={styles.principal}>
             <Outlet />
