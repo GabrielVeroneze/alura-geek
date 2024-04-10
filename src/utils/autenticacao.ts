57f28e80d28312bf76048b9e3e69b61ed5b1194f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-09 23:21:29 -0300

    :sparkles: feat: Simula os dados retornados pelas requisições de autenticação
    Cria funções para simular os dados retornados pelas requisições de autenticação

--- src/utils/autenticacao.ts
+++ src/utils/autenticacao.ts
@@ -1,3 +1,35 @@
+import { nanoid } from 'nanoid'
+import { IUsuario } from '@/interfaces/IUsuario'
 
+let usuario: IUsuario | null = null
 
+export const simularRequisicaoValidarToken = async (token: string) => {
+    return {
+        data: {
+            usuario,
+        },
+    }
+}
 
+export const simularRequisicaoLogin = async (email: string, senha: string) => {
+    usuario = {
+        id: nanoid(9),
+        email: email,
+        senha: senha,
+    }
+
+    return {
+        data: {
+            token: nanoid(17),
+            usuario,
+        },
+    }
+}
+
+export const simularRequisicaoLogout = async () => {
+    return {
+        data: {
+            status: true,
+        },
+    }
+}
