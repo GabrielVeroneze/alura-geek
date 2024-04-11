7992f770279f028308983af967234c77471c0f9b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-11 20:13:03 -0300

    :sparkles: feat: Cria componente para os produtos em destaque

---
+++ src/components/GrupoProdutosDestaque/index.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom'
+import { IProduto } from '@/interfaces/IProduto'
+import Produto from '@/components/Produto'
+import styles from './GrupoProdutosDestaque.module.scss'
+
+interface GrupoProdutosDestaqueProps {
+    categoria: string
+    produtos: IProduto[]
+}
+
+const GrupoProdutosDestaque = ({ categoria, produtos }: GrupoProdutosDestaqueProps) => {
+    return (
+        <section id={categoria} className={styles.grupo}>
+            <div className={styles.topo}>
+                <h3 className={styles.titulo}>{categoria}</h3>
+                <Link to={`/produtos/${categoria}`} className={styles.botao}>
+                    Ver tudo
+                </Link>
+            </div>
+            <ul className={styles.lista}>
+                {produtos.map(produto => (
+                    <Produto
+                        key={produto.id}
+                        modelo="visualizar"
+                        produto={produto}
+                    />
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+export default GrupoProdutosDestaque
