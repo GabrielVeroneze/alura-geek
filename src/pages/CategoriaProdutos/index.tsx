e5fd7cfa39c936852dcafc04203d98d77dd994ca
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-11 17:46:20 -0300

    :sparkles: feat: Cria página para os produtos de uma categoria
    Adiciona uma nova página para todos os produtos de uma categoria especifica.

---
+++ src/pages/CategoriaProdutos/index.tsx
@@ -0,0 +1,28 @@
+import { useParams } from 'react-router-dom'
+import { useManipularProdutos } from '@/hooks/useManipularProdutos'
+import { filtrarProdutosPorCategoria } from '@/utils/operacoesCategorias'
+import GrupoProdutos from '@/components/GrupoProdutos'
+import styles from './CategoriaProdutos.module.scss'
+
+const CategoriaProdutos = () => {
+    const { produtos } = useManipularProdutos()
+    const { categoria } = useParams()
+
+    const categoriaValida = categoria || ''
+
+    return (
+        <main className={styles.principal}>
+            <GrupoProdutos
+                modelo="relacionado"
+                categoria={`Todos os Produtos - ${categoria}`}
+                produtos={filtrarProdutosPorCategoria(
+                    produtos,
+                    categoriaValida,
+                    produtos.length
+                )}
+            />
+        </main>
+    )
+}
+
+export default CategoriaProdutos
