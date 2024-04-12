4910b743edb3ffd3dd22ad1214bf3c8b6a3e0128
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-11 21:13:27 -0300

    :wastebasket: remove: Remove modelos do componente

--- src/components/GrupoProdutos/index.tsx
+++ src/components/GrupoProdutos/index.tsx
@@ -1,33 +1,23 @@
-import { Link } from 'react-router-dom'
 import { IProduto } from '@/interfaces/IProduto'
-import Produto from './Produto'
+import Produto from '@/components/Produto'
 import styles from './GrupoProdutos.module.scss'
 
 interface GrupoProdutosProps {
-    modelo: 'destaque' | 'relacionado' | 'editar'
     categoria: string
     produtos: IProduto[]
 }
 
-const GrupoProdutos = ({ modelo, categoria, produtos }: GrupoProdutosProps) => {
+const GrupoProdutos = ({ categoria, produtos }: GrupoProdutosProps) => {
     return (
-        <section id={categoria} className={`${styles.grupo} ${styles[modelo]}`}>
+        <section id={categoria} className={styles.grupo}>
             <div className={styles.topo}>
-                <h3 className={styles.categoria}>{categoria}</h3>
-                {modelo === 'destaque' && (
-                    <button className={styles.botao}>Ver tudo</button>
-                )}
-                {modelo === 'editar' && (
-                    <Link to="/admin/novo-produto" className={styles.botao}>
-                        Adicionar produto
-                    </Link>
-                )}
+                <h3 className={styles.titulo}>{categoria}</h3>
             </div>
             <ul className={styles.lista}>
                 {produtos.map(produto => (
                     <Produto
                         key={produto.id}
-                        modelo={modelo === 'editar' ? 'editar' : 'visualizar'}
+                        modelo="visualizar"
                         produto={produto}
                     />
                 ))}
