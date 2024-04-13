806fbb0e727e63cd95971f8ceb6b56eafdaca251
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-13 16:45:53 -0300

    :wastebasket: remove: Remove seletor assíncrono

--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -1,17 +1,6 @@
 import { selector } from 'recoil'
-import { jsonServerApi } from '@/services/api'
 import { produtosAtom, termoDePesquisaAtom } from '@/state/atoms'
 import { IProduto } from '@/interfaces/IProduto'
-
-export const produtosAsyncSelector = selector<IProduto[]>({
-    key: 'produtosAsyncSelector',
-    get: async () => {
-        const resposta = await jsonServerApi.get<IProduto[]>('produtos')
-        const dados = resposta.data
-
-        return dados
-    },
-})
 
 export const produtosPesquisadosSelector = selector<IProduto[]>({
     key: 'produtosPesquisadosSelector',
