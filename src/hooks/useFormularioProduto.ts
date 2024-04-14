c7690b2d11e53835822a3763638a060338cdc2d4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-13 23:34:42 -0300

    :sparkles: feat: Adiciona função que atualiza estado ao return

--- src/hooks/useFormularioProduto.ts
+++ src/hooks/useFormularioProduto.ts
@@ -47,6 +47,7 @@ export const useFormularioProduto = () => {
 
     return {
         produtoDados,
+        setProdutoDados,
         handleDadosChange,
         handleSubmit,
         mascaraMonetaria,
