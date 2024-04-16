5287d7a4af092f94d878fdeca77dc5d56bf8eab3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-04-15 22:02:09 -0300

    :sparkles: feat: Passa função removerProduto para GrupoProdutosEditavel

--- src/pages/Administracao/MenuAdmin/index.tsx
+++ src/pages/Administracao/MenuAdmin/index.tsx
@@ -2,12 +2,13 @@ import { useManipularProdutos } from '@/hooks/useManipularProdutos'
 import GrupoProdutosEditavel from '@/components/GrupoProdutosEditavel'
 
 const MenuAdmin = () => {
-    const { produtos } = useManipularProdutos()
+    const { produtos, removerProduto } = useManipularProdutos()
 
     return (
         <GrupoProdutosEditavel
             categoria="Todos os produtos"
             produtos={produtos}
+            removerProduto={removerProduto}
         />
     )
 }
