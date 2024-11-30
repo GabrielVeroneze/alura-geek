9c373ba2178d395a089765b5e8d11a1979277851
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-11-30 18:13:53 -0300

    ♻️ refactor: Adiciona tipagem explícita ao login

--- src/services/autenticacao.ts
+++ src/services/autenticacao.ts
@@ -1,8 +1,9 @@
 import api from '@/services/api'
+import { Usuario } from '@/types/Usuario'
 
-export async function login(email: string, senha: string) {
+export async function login(email: string, senha: string): Promise<Usuario> {
     try {
-        const resposta = await api.post('/usuarios', { email, senha })
+        const resposta = await api.post<Usuario>('/usuarios', { email, senha })
         return resposta.data
     } catch {
         throw new Error('Erro ao realizar o login.')
